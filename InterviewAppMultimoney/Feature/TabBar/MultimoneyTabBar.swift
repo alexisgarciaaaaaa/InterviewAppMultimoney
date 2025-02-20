a493c5564713b0ae4f923e31c0f17400ea197552
bryangarcia <alexisgarciathb1996@gmail.com>
2025-02-19 19:29:01 -0600

    feat: Implement

---
+++ InterviewAppMultimoney/Feature/TabBar/MultimoneyTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  MultimoneyTabBar.swift
+//  InterviewAppMultimoney
+//
+//  Created by bryangarcia on 19/2/25.
+//
+
+import SwiftUI
+
+struct MultimoneyTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultimoneyTabBar()
+}
